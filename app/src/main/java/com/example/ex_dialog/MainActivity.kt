0e6d833773043e314d40f51b142fa8a77e10f3f8
4seokhyeon <tktjrgus@naver.com>
2023-08-22 17:06:27 +0900

    기본 다이얼로그, 커스텀 다이얼로그, 날짜 다이얼로그

--- app/src/main/java/com/example/ex_dialog/MainActivity.kt
+++ app/src/main/java/com/example/ex_dialog/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.ex_dialog
 
+import android.app.DatePickerDialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.AlteredCharSequence
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.example.ex_dialog.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         // 1. 기본 다이얼로그
         binding.btn1Alert.setOnClickListener {
             var builder = AlertDialog.Builder(this)
@@ -38,6 +42,44 @@ class MainActivity : AppCompatActivity() {
             builder.setNeutralButton("Neutral", listener)
 
             builder.show()
+        }
+
+        // 2. 커스텀 다이얼로그
+        binding.btn2Custom.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("커스텀 다이얼로그")
+            builder.setIcon(R.mipmap.ic_launcher)
+
+            val v1 = layoutInflater.inflate(R.layout.dialog, null)
+            builder.setView(v1)
+
+            // p0에 해당 AlertDialog가 들어온다. findViewById를 통해 view를 가져와서 사용
+            val listener = DialogInterface.OnClickListener { p0, p1 ->
+                val alert = p0 as AlertDialog
+                val edit1: EditText? = alert.findViewById<EditText>(R.id.editText)
+                val edit2: EditText? = alert.findViewById<EditText>(R.id.editText2)
+
+                binding.tvTitle.text = "이름 : ${edit1?.text}"
+                binding.tvTitle.append(" / 나이 : ${edit2?.text}")
+            }
+
+            builder.setPositiveButton("확인", listener)
+            builder.setNegativeButton("취소", null)
+
+            builder.show()
+        }
+
+        binding.btn3Date.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val listener = DatePickerDialog.OnDateSetListener{datePicker, i, i2, i3 ->
+                binding.tvTitle.text = "${i}년 ${i2 + 1}월 ${i3}일"
+            }
+            var picker = DatePickerDialog(this,listener,year,month,day)
+            picker.show()
         }
 
 
